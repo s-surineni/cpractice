90e8711272be7366beb4d51dfc8ebd58428e46f1
vinnu <sampath.surineni@gmail.com>
2013-09-24 14:40:42 +0530

    problem1

--- ll.c
+++ ll.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-<<<<<<< HEAD
 #include<stdlib.h>
 void insert();
 void display();
@@ -61,72 +60,4 @@ item++;}
     }
 return;
 }
-=======
-void insert(void);
 
-struct node {
-    int x;
-    struct node *next;
-};
-void traverse(struct node *conductor);
-
-int main() {
-    char wtd;
-    struct node *root;
-    while(wtd!='1'){
-        printf("hi user\n");
-        printf("enter\n");
-        printf("\t i to insert\n");
-        switch(wtd){
-            case 'i':
-                insert();
-                break;
-        }
-        
-    }
-    
-    root = malloc(sizeof (struct node));
-    root->x = 10;
-    printf("%d\n", root->x);
-    struct node *conductor;
-    conductor = malloc(sizeof (struct node));
-    conductor->x = 20;
-    struct node s1;
-    root->next = &s1;
-    s1.x = 30;
-    printf("%d\n", s1.x);
-    struct node *ps1;
-    ps1 = &s1;
-    printf("%d\n", (*ps1).x);
-    struct node s2;
-    s2.x = 40;
-    s1.next = &s2;
-    struct node s3;
-    s3.x = 50;
-    s2.next = &s3;
-    struct node s4;
-    s4.x = 60;
-    s3.next = &s4;
-    s4.next = NULL;
-    traverse(root);
-    return 0;
-}
-
-void traverse(struct node *conductor) {
-
-    for (; conductor->next != NULL;) {
-        printf("%d \n ", conductor->x);
-        conductor = conductor->next;
-    }
-}
-void insert(){
-    struct node create,*pcreate;
-    *pcreate=&create;
-    int enter;
-    printf("enter the valued to be inserted ");
-    scanf("%d",&enter);
-    create.x=enter;
-    create.next=NULL;
-    
-}
->>>>>>> 9143bd16b3ac1e2e96b4fdeebb057a9c848f5798
