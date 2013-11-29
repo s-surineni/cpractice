48852d8c7bcafe58ea8026c18d35789cb21242b8
sampath <sampath.surineni@gmail.com>
2013-11-28 23:29:04 -0800

    printed addresses

--- p1.cpp
+++ p1.cpp
@@ -2,4 +2,8 @@
 int main(){
 	int v1;
 	int a[10];
+	printf("the address of v1 is %x \n",&v1);
+	printf("the address of a is %x\n",&a);
+	printf("the address of v1 is %d \n",&v1);//the address in integers
+	printf("the address of v1 is %f \n",&v1);
 }
